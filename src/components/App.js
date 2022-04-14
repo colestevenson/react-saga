17d78c10c600d73f22d6b737d079dd0943d29b0c
colestevenson <cole@newmoyergeospatial.com>
2022-04-14 15:32:20 -0400

    Lectures 8-13 - setting up the users saga with call, fork, and put.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getUsersRequest} from '../actions/users';
 
-function App() {
-  return (
-    <div>test</div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.props.getUsersRequest();
+  }
+
+  render() {
+    return (
+      <div>test</div>
+    );
+  }
 }
 
-export default App;
+export default connect(null, {
+  getUsersRequest
+})(App);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from './components/App';
 import axios from 'axios';
 import reducers from './reducers';
 import {Provider} from 'react-redux';
-import {CreateStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './sagas';
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api'
+axios.defaults.baseURL = 'http://rem-rest-api.herokuapp.com/api';
 
-const store = CreateStore.createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
